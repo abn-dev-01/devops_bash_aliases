10fd705f0427444aa2571da6544ce8741e532a76
Andrew Nikitin <abn.java@gmail.com>
2024-03-08 16:15:32 +0100

    add GIT aliases

--- abn.git.sh
+++ abn.git.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 #
-# <> aliases
+# GIT commands aliases
 #
+alias gi-add-it='git add .'
+alias gi-remote-add='git remote add '
+alias gi-push='git push '
+alias gi-status='git status'
+alias gi-commit='git commit'
+alias gi-commit-auto='git commit -a'
+alias gi-help='git --help'
+
+alias gi-coglobal='git config --global '
+alias gi-coglobal-user-email='git config --global user.email '
+alias gi-coglobal-user-name='git config --global user.name '
+
+alias gi-merhelp='git merge --help'
+alias gi-mersguash='git merge --squash '
+
+alias gi-rebase-help='git rebase --help'
+
 
