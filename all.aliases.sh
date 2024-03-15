65f0138d02f6c54103804205499771fb09a0ee8f
ABN.dev.01 <49402373+abn-dev-01@users.noreply.github.com>
2024-03-15 15:40:12 +0300

    Create all.aliases.sh

---
+++ all.aliases.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+##############################################################
+# Import environment variables from files
+source ~/.bp/abn.system.sh
+source ~/.bp/abn.vim.sh
+source ~/.bp/abn.sudo.sh
+source ~/.bp/abn.docker.sh
+source ~/.bp/abn.tail.sh
+source ~/.bp/abn.iptables.sh
+source ~/.bp/abn.git.sh
