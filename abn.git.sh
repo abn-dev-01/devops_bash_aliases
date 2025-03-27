5b02f72f7a4c5fe407a400244644fc3d17b63309
ABN.dev.01 <49402373+abn-dev-01@users.noreply.github.com>
2025-03-27 19:01:26 -0300

    Update abn.git.sh
    add git settings

--- abn.git.sh
+++ abn.git.sh
@@ -10,13 +10,29 @@ alias gi-commit='git commit'
 alias gi-commit-auto='git commit -a'
 alias gi-help='git --help'
 
+# configs 
 alias gi-coglobal='git config --global '
-alias gi-coglobal-user-email='git config --global user.email '
-alias gi-coglobal-user-name='git config --global user.name '
+alias gi-conf-global-user-email='git config --global user.email '
+alias gi-conf-global-user-name='git config --global user.name '
+alias gi-conf-user-email='git config user.email '
+alias gi-conf-user-name='git config user.name '
+alias gi-conf-user-email-abndev01='git config user.email "abn.dev.01@gmail.com" '
+alias gi-conf-user-name-AndreyNikitin='git config user.name "Andrei Nikitin" '
 
 alias gi-merhelp='git merge --help'
 alias gi-mersguash='git merge --squash '
 
 alias gi-rebase-help='git rebase --help'
+
+alias gi-stash-untracked='git stash push -u -m '
+alias gi-stash='git stash push -u -m '
+
+# checkout`s 
+alias gi-chk-new-branch='git checkout -b '
+
+# comparing
+alias gi-cmpr-head='git diff HEAD -- '
+
+
 
 
