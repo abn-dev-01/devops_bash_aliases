920016207312d72f155ae37c3f8f24f154223256
ABN.dev.01 <49402373+abn-dev-01@users.noreply.github.com>
2025-03-28 08:32:51 -0300

    Update all.aliases.sh

--- all.aliases.sh
+++ all.aliases.sh
@@ -9,5 +9,7 @@ source ~/.bp/abn.tail.sh
 source ~/.bp/abn.iptables.sh
 source ~/.bp/abn.git.sh
 source ~/.bp/abn.paths.sh
-source ~/.bp/abn.microk8s.sh
+# microk8s is before k8s! file
+#source ~/.bp/abn.microk8s.sh
+source ~/.bp/abn.k8s.sh
 source ~/.bp/abn.devops.sh
