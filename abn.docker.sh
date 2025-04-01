f285782b1b59287c3a744508c480a56b82c51410
ABN.dev.01 <49402373+abn-dev-01@users.noreply.github.com>
2025-04-01 19:21:27 -0300

    Update abn.docker.sh

--- abn.docker.sh
+++ abn.docker.sh
@@ -4,12 +4,26 @@
 #
 # <<< INSERT HERE DOCKER ALIASES >> #
 
-alias tailcrm-telecom999='tail -n 999 -f ~/logs/telecom/crm-telecom-*'
-alias tailcrm-rest2222='tail -n 2222 -f ~/logs/crm-rest/logs/*the_cloud.log'
-alias tailcrm-monitoring='tail -n 999 -f ~/logs/monitoring/mappers-monitoring.log ~/logs/monitoring/the_cloud_monitoring.log'
+alias   dkstop='docker stop'
+alias   dkpul='docker pull'
+alias   dkps='docker ps'
+alias   dkpsl='docker ps -l'
+alias   dkimages='docker images'
+alias   dkimgls='docker images'
+ alias dk__rm_volumes=' docker volume rm $(docker volume ls -q)'
 
-alias pxcc='ping x.cc'
-alias oldtc8startUp='/home/intetics/OLD_CRM/apache-tomcat-8.0.23/bin/startup.sh'
-alias oldtc8catalina-stop='/home/intetics/OLD_CRM/apache-tomcat-8.0.23/bin/catalina.sh stop'
+alias   dkcont_ps_a='docker container ps -a'
+alias   dkcont_rmf='docker container rm -f'
 
-alias pxcc='ping x.cc'
+alias   dkbuild='docker build -f Dockerfile -t'
+alias   dkexecit='docker exec it'
+
+alias   dkcontlscreated='docker container ls -a --filter status=exited --filter status=created'
+#        alias   dkrun-1cpg='docker run --rm --name 1c-pgsql -e POSTGRES_PASSWORD=docker -v /srv/pgsql/1c:/var/lib/postgresql/data -d -p 5432:5432 postgres:11.6'
+
+alias   dc='docker-compose'
+alias   dclogs='docker-compose logs -f --tail=1000'
+alias   dcps='docker-compose ps'
+alias   dcimgs='docker-compose images'
+alias   dcup-d='docker-compose up -d '
+alias   dcup='docker-compose up '
